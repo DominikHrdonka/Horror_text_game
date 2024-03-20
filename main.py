e18d7bcf244d0adc6d09430aaf66d1c7b341ccb9
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-03-20 08:34:38 +0100

    added the Game class and a method to start the game.

---
+++ main.py
@@ -0,0 +1,16 @@
+def main():
+    class Game():
+        def __init__(self):
+            self.game_over = False
+            
+        def start_game(self):
+            print("You open your eyes slowly. Your head is throbbing.")
+            print("The room is dimly lit, not anything like your memory.")
+            print("That is lost in a black hole of oblivion.")
+            print("You get up slowly from the floor. The wood creaks beneath your feet.")
+            print("You need to find your way out of here.")
+    
+    game = Game()
+    game.start_game()
+
+main()

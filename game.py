60145233dbf23b78012ff6a9c91b5e46d2cc3a73
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-03-20 11:38:22 +0100

    added method to add location instances in a dictionary, add method to change current location and the corresponding choices the user can make

--- game.py
+++ game.py
@@ -3,6 +3,18 @@ class Game:
     def __init__(self):
         self.game_over = False
         self.current_location = None
+        self.locations = {}
+    ### method to add Location instances to the dictionary
+    def add_location(self, key, location):
+        self.locations[key] = location
+
+    ### method to change current location
+    def change_location(self, key):
+        if key in self.locations:
+            self.current_location = self.locations[key]
+        else:
+            print("Location doesn't exist")
+            ### make a loop to repeat when wrong output
             
     def start_game(self):
         print("You open your eyes slowly. Your head is throbbing.")
@@ -28,10 +40,10 @@ class Game:
     def open_the_wardrobe(self):
         print("You decided to open the wardrobe!")
         print("---------------------------------")
-        print("The wardrobe creaks. Awful smell gets out.\nYou feel sick and have to cover your nose.\nAs the shock passes, you notice something inside.\nA ragged doll with one eye. And there... a key!")
+        self.change_location("wardrobe")
+        print(f"{self.current_location.description}")
         
 
-        
     def main_loop(self, user_input):
         while not self.game_over:
             if user_input == "explore":
@@ -45,7 +57,8 @@ class Game:
             elif user_input == "open the wardrobe":
                 user_input = None
                 self.open_the_wardrobe()
-                ### change locatio to get new choices
+
+                ### change location to get new choices
 
             elif user_input == "quit":
                 self.game_over = True
--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from game import Game
 from location import Location
-
 def main():
     
     # Creating instances
     game = Game()
-    dark_room = Location(name = "Dark Room", description = "As your eyes get used to the dark,\nyou start to distinguish a dark fram set on the opposite wall.\nA door! Next it there's a black shadow of a wardrobe lurking,\nsitting quietly. The walls are empty. The air heavy with dust.", choices = "open the door, open the wardrobe")
+    dark_room = Location(
+        "Dark Room",
+        "As your eyes get used to the dark,\nyou start to distinguish a dark fram set on the opposite wall.\nA door! Next it there's a black shadow of a wardrobe lurking,\nsitting quietly. The walls are empty. The air heavy with dust.",
+        "open the door, open the wardrobe"
+        )
+
+    wardrobe = Location(
+        "wardrobe",
+        "The wardrobe creaks. Awful smell gets out.\nYou feel sick and have to cover your nose.\nAs the shock passes, you notice something inside.\nA ragged doll with one eye. And there... a key!",
+        "take the key, take the doll"
+        )
+
+    # Adding location to the game list of locations
+    game.add_location("dark room", dark_room)
+    game.add_location("wardrobe", wardrobe)
 
     # Defining current location
     game.current_location = dark_room
+
     # Actual game progress
     game.start_game()
 

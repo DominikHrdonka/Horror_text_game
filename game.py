a01d2fdcab01c345ea6927ebe3b9dd7cc1bde934
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-03-20 10:12:23 +0100

    set up the current location and integrated it to the choice making

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
-class Game():
+from location import Location
+class Game:
     def __init__(self):
         self.game_over = False
+        self.current_location = None
             
     def start_game(self):
         print("You open your eyes slowly. Your head is throbbing.")
@@ -8,6 +10,17 @@ class Game():
         print("That is lost in a black hole of oblivion.")
         print("You get up slowly from the floor. The wood creaks beneath your feet.")
         print("You need to find your way out of here.")
+        self.main_loop()
+    
+    def explore(self):
+        print("You decided to explore!")
+        print(f"{self.current_location.description}") 
         
-    def user_input():
-        user_input = input("What do you want to do? (explore, quit).")
+    def main_loop(self):
+        while not self.game_over:
+            user_input = input("What do you want to do? (explore, quit) > ")
+            if user_input == "explore":
+                self.explore()
+            if user_input == "quit":
+                self.game_over = True
+
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from game import Game
+from location import Location
 
 def main():
     
-    # The actual game progress
+    # Creating instances
     game = Game()
+    dark_room = Location(name = "Dark Room", description = "As your eyes get used to the dark,\n you start to distinguish a dark fram set on the opposite wall.\nA door! Next it there's a black shadow of a wardrobe lurking,\nsitting quietly. Tha walls are empty. The air heavy with dust.")
+
+    # Defining current location
+    game.current_location = dark_room
+    # Actual game progress
     game.start_game()
 
-main()
+
+
+if __name__ == "__main__":
+    main()

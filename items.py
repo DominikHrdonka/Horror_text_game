b5935747370650737663e6f1ba903144a967c35d
Dominik Hrdonka <hrdonka.d@gmail.com>
2024-03-31 10:16:12 +0200

    created new child class of Weapons, added method to craft Items together

--- items.py
+++ items.py
@@ -1,3 +1,16 @@
 class Items:
     def __init__(self, name) -> None:
         self.name = name
+
+    def __add__(self, other):
+        pass
+
+class Weapons(Items):
+    def __init__(self, name, power):
+        super().__init__(name)
+        self.power = power
+            
+
+key = Items("key")
+scalpel = Weapons("scalpel", 2)
+axe = Weapons("axe", 7)
